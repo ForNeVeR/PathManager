22aff1336f19174c8cd61eb8d3568eab3f5dc27d
ForNeVeR <neverthness@gmail.com>
2013-10-13 20:35:44 +0700

    Initial commit.

---
+++ PathManager/EnvironmentViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace PathManager
+{
+	public class EnvironmentViewModel : ReactiveObject
+	{
+		private const string PathVariable = "PATH";
+
+		private ObservableCollection<PathViewModel> _paths;
+
+		public EnvironmentViewModel()
+		{
+			RefreshCommand = new ReactiveCommand();
+			RefreshCommand.Subscribe(_ => Refresh());
+		}
+
+		public ObservableCollection<PathViewModel> Paths
+		{
+			get
+			{
+				return _paths;
+			}
+			private set
+			{
+				this.RaiseAndSetIfChanged(ref _paths, value);
+			}
+		}
+
+		public ReactiveCommand RefreshCommand
+		{
+			get;
+			private set;
+		}
+
+		private static IEnumerable<PathViewModel> GetPaths(EnvironmentVariableTarget mode, SettingType settingType)
+		{
+			string variable = Environment.GetEnvironmentVariable(PathVariable, mode);
+			if (variable == null)
+			{
+				return Enumerable.Empty<PathViewModel>();
+			}
+
+			return variable
+				.Split(Path.PathSeparator)
+				.Select(path => new PathViewModel
+				{
+					SettingType = settingType,
+					Path = path,
+					IsChanged = false
+				});
+		}
+
+		private void Refresh()
+		{
+			var userPaths = GetPaths(EnvironmentVariableTarget.User, SettingType.User);
+			var systemPaths = GetPaths(EnvironmentVariableTarget.Machine, SettingType.System);
+
+			Paths = new ObservableCollection<PathViewModel>(userPaths.Concat(systemPaths));
+		}
+	}
+}
---
+++ PathManager/PathViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace PathManager
+{
+	public class PathViewModel : ReactiveObject
+	{
+		private string _path;
+		private bool _changed;
+		private SettingType _settingType;
+		private string _changedText;
+
+		public PathViewModel()
+		{
+			this.ObservableForProperty(t => t.Path)
+				.Subscribe(c => { c.Sender.IsChanged = true; });
+
+			this.ObservableForProperty(t => t.IsChanged)
+				.Subscribe(c => { c.Sender.ChangedText = c.Value ? "(changed)" : "(not changed)"; });
+		}
+
+		public bool IsChanged
+		{
+			get { return _changed; }
+			set { this.RaiseAndSetIfChanged(ref _changed, value); }
+		}
+
+		public string Path
+		{
+			get { return _path; }
+			set { this.RaiseAndSetIfChanged(ref _path, value); }
+		}
+
+		public SettingType SettingType
+		{
+			get { return _settingType; }
+			set { this.RaiseAndSetIfChanged(ref _settingType, value); }
+		}
+
+		public string ChangedText
+		{
+			get { return _changedText; }
+			private set { this.RaiseAndSetIfChanged(ref _changedText, value); }
+		}
+	}
+}
